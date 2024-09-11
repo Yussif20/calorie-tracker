45cc1c5fd2cd4719e5f99988d2c32140c93f78e4
Yousef ayman <yousefayman2000500@gmail.com>
2024-09-11 08:42:03 +0300

    adding calorie record component

---
+++ src/components/CalorieRecord.jsx
@@ -0,0 +1,12 @@
+import "./CalorieRecord.css"
+const CalorieRecord = () =>{
+    return(
+        <ul className="record">
+            <li>September 11, 2024</li>
+            <li>Breakfast</li>
+            <li>Eggs + Beans</li>
+            <li className="record-calories">340</li>
+        </ul>
+    )
+}
+export default CalorieRecord;
