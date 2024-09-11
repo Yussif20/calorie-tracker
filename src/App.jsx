b53629bff0d071500840bc71726e43147f4d553e
Yousef ayman <yousefayman2000500@gmail.com>
2024-09-11 09:21:31 +0300

    passing data to the components via props

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ function App() {
   return (
     <>
      <h1>Hello to my calorie tracker</h1>
-    <CalorieRecord></CalorieRecord>
+    <CalorieRecord date={new Date(2024,8,6)} meal="Breakfast" content="Eggs" calories="340"></CalorieRecord>
+    <CalorieRecord date={new Date(2024,8,5)} meal="Launch" content="Chicken" calories="400"></CalorieRecord>
+    <CalorieRecord date={new Date(2024,8,4)} meal="Dinner" content="Cheese" calories="450"></CalorieRecord>
+    <CalorieRecord date={new Date(2024,8,3)} meal="Snacks" content="Chocolate" calories="500"></CalorieRecord>
     </>
   )
 }
--- src/components/CalorieRecord.jsx
+++ src/components/CalorieRecord.jsx
@@ -1,11 +1,11 @@
 import "./CalorieRecord.css"
-const CalorieRecord = () =>{
+const CalorieRecord = (props) =>{
     return(
         <ul className="record">
-            <li>September 11, 2024</li>
-            <li>Breakfast</li>
-            <li>Eggs + Beans</li>
-            <li className="record-calories">340</li>
+            <li>{props.date.toISOString()}</li>
+            <li>{props.meal}</li>
+            <li>{props.content}</li>
+            <li className="record-calories">{props.calories}</li>
         </ul>
     )
 }
