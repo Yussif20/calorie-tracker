b3b089870fbdc720266177e9e290fff3550728f0
Yousef ayman <yousefayman2000500@gmail.com>
2024-09-13 04:22:20 +0300

    handling user input

--- src/components/calorieRecordEdit/CaloriesRecordForm.jsx
+++ src/components/calorieRecordEdit/CaloriesRecordForm.jsx
@@ -1,23 +1,53 @@
+import { useState } from "react"
 import "./CaloriesRecordForm.css"
 
 const CaloriesRecordForm = () => {
+  const [mealRecord,setMealRecord]=useState({});
+  const onDateChangeHandler = (event)=>{
+    setMealRecord({
+      ...mealRecord,
+      date: event.target.value,
+    })
+  }
+  const onMealChangeHandler = (event)=>{
+    setMealRecord({
+      ...mealRecord,
+      meal: event.target.value,
+    })
+  }
+  const onContentChangeHandler = (event)=>{
+    setMealRecord({
+      ...mealRecord,
+      content: event.target.value,
+    })
+  }
+  const onCaloriesChangeHandler = (event)=>{
+    setMealRecord({
+      ...mealRecord,
+      calories: event.target.value,
+    })
+  }
+  const onSubmitHandler = (event)=>{
+    event.preventDefault();
+    console.log(mealRecord)
+  }
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
         <label htmlFor="date">Date:</label>
-        <input type="date" name="date" id="date"/>
+        <input type="date" name="date" id="date" onChange={onDateChangeHandler}/>
         <label htmlFor="meal">Meal:</label>
-        <select name="meal" id="meal">
+        <select name="meal" id="meal" onChange={onMealChangeHandler}>
             <option value="Breakfast">Breakfast</option>
             <option value="Launch">Launch</option>
             <option value="Dinner">Dinner</option>
             <option value="Snacks">Snacks</option>
         </select>
         <label htmlFor="content">Content:</label>
-        <input type="text" name="content" id="content"/>
+        <input type="text" name="content" id="content" onChange={onContentChangeHandler}/>
         <label htmlFor="calories">Calories:</label>
-        <input type="number" name="=calories" id="=calories"/>
+        <input type="number" name="=calories" id="calories" onChange={onCaloriesChangeHandler}/>
         <div className="footer">
-            <button>Add Record</button>
+            <button >Add Record</button>
         </div>
     </form>
   )
