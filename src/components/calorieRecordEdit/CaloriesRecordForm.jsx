76d0ae15f4138ca78575756a23cf69851b6760e2
Yousef ayman <yousefayman2000500@gmail.com>
2024-09-13 07:51:30 +0300

    using the two way binding to handle user input more correctly

--- src/components/calorieRecordEdit/CaloriesRecordForm.jsx
+++ src/components/calorieRecordEdit/CaloriesRecordForm.jsx
@@ -1,56 +1,86 @@
-import { useState } from "react"
-import "./CaloriesRecordForm.css"
+import { useState } from 'react';
+import './CaloriesRecordForm.css';
 
 const CaloriesRecordForm = () => {
-  const [mealRecord,setMealRecord]=useState({});
-  const onDateChangeHandler = (event)=>{
+  const DEFAULT_VALUE = {
+    date: '',
+    meal: '',
+    content: '',
+    calories: 0,
+  };
+  const [mealRecord, setMealRecord] = useState(DEFAULT_VALUE);
+  const onDateChangeHandler = (event) => {
     setMealRecord({
       ...mealRecord,
       date: event.target.value,
-    })
-  }
-  const onMealChangeHandler = (event)=>{
+    });
+  };
+  const onMealChangeHandler = (event) => {
     setMealRecord({
       ...mealRecord,
       meal: event.target.value,
-    })
-  }
-  const onContentChangeHandler = (event)=>{
+    });
+  };
+  const onContentChangeHandler = (event) => {
     setMealRecord({
       ...mealRecord,
       content: event.target.value,
-    })
-  }
-  const onCaloriesChangeHandler = (event)=>{
+    });
+  };
+  const onCaloriesChangeHandler = (event) => {
     setMealRecord({
       ...mealRecord,
       calories: event.target.value,
-    })
-  }
-  const onSubmitHandler = (event)=>{
+    });
+  };
+  const onSubmitHandler = (event) => {
     event.preventDefault();
-    console.log(mealRecord)
-  }
+    setMealRecord(DEFAULT_VALUE);
+    console.log(mealRecord);
+  };
   return (
     <form onSubmit={onSubmitHandler}>
-        <label htmlFor="date">Date:</label>
-        <input type="date" name="date" id="date" onChange={onDateChangeHandler}/>
-        <label htmlFor="meal">Meal:</label>
-        <select name="meal" id="meal" onChange={onMealChangeHandler}>
-            <option value="Breakfast">Breakfast</option>
-            <option value="Launch">Launch</option>
-            <option value="Dinner">Dinner</option>
-            <option value="Snacks">Snacks</option>
-        </select>
-        <label htmlFor="content">Content:</label>
-        <input type="text" name="content" id="content" onChange={onContentChangeHandler}/>
-        <label htmlFor="calories">Calories:</label>
-        <input type="number" name="=calories" id="calories" onChange={onCaloriesChangeHandler}/>
-        <div className="footer">
-            <button >Add Record</button>
-        </div>
+      <label htmlFor="date">Date:</label>
+      <input
+        value={mealRecord.date}
+        type="date"
+        name="date"
+        id="date"
+        onChange={onDateChangeHandler}
+      />
+      <label htmlFor="meal">Meal:</label>
+      <select
+        value={mealRecord.meal}
+        name="meal"
+        id="meal"
+        onChange={onMealChangeHandler}
+      >
+        <option value="Breakfast">Breakfast</option>
+        <option value="Launch">Launch</option>
+        <option value="Dinner">Dinner</option>
+        <option value="Snacks">Snacks</option>
+      </select>
+      <label htmlFor="content">Content:</label>
+      <input
+        value={mealRecord.content}
+        type="text"
+        name="content"
+        id="content"
+        onChange={onContentChangeHandler}
+      />
+      <label htmlFor="calories">Calories:</label>
+      <input
+        value={mealRecord.calories}
+        type="number"
+        name="=calories"
+        id="calories"
+        onChange={onCaloriesChangeHandler}
+      />
+      <div className="footer">
+        <button>Add Record</button>
+      </div>
     </form>
-  )
-}
+  );
+};
 
-export default CaloriesRecordForm
+export default CaloriesRecordForm;
--- src/components/calorieRecordSection/RecordList.jsx
+++ src/components/calorieRecordSection/RecordList.jsx
@@ -1,16 +1,23 @@
-import CalorieRecord from "./CalorieRecord"
-import "./RecordList.css"
+import CalorieRecord from './CalorieRecord';
+import './RecordList.css';
 
 const RecordList = (props) => {
   return (
-    <ul className="record-list">{props.records.map((record)=>{
+    <ul className="record-list">
+      {props.records.map((record) => {
         return (
-            <li key={record.id}>
-                <CalorieRecord  date={record.date} meal={record.meal} content={record.content} calories={record.calories}/>
-            </li>
-            )
-    })}</ul>
-  )
-}
+          <li key={record.id}>
+            <CalorieRecord
+              date={record.date}
+              meal={record.meal}
+              content={record.content}
+              calories={record.calories}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-export default RecordList
+export default RecordList;
