0b9febaeab61ce43ad2322a203d8251289004aa2
Yousef ayman <yousefayman2000500@gmail.com>
2024-09-30 16:44:18 +0300

    navigate programmaticly to home page when in error

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,40 @@
-import { Link } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styles from './ErrorPage.module.css';
+
+const INTERVAL_TIME = 1000;
+const COUNTER = 10;
+const HOME_LINK = '/';
 
 export const ErrorPage = () => {
+  const [counter, setCounter] = useState(COUNTER);
+  const intervalHandler = useRef();
+  const NavigateToHome = useNavigate();
+
+  useEffect(() => {
+    if (counter === 0) {
+      clearInterval();
+      NavigateToHome(HOME_LINK);
+    }
+  }, [counter]);
+
+  useEffect(() => {
+    intervalHandler.current = setInterval(() => {
+      setCounter((prevState) => prevState - 1);
+    }, INTERVAL_TIME);
+
+    return () => {
+      clearInterval(intervalHandler.current);
+    };
+  }, []);
+
   return (
-    <>
-      <h1>Something Went Wrong...</h1>
+    <div className={styles['error-container']}>
+      <h1> Something Went Wrong :(</h1>
+      <p>Redirecting to home page in {counter}</p>
       <p>
-        Click <Link to="/">Here</Link> to go back
+        Or click <Link to="/">Home Page</Link> to go back
       </p>
-    </>
+    </div>
   );
 };
